7e3750e41d4977b847abbdfeb5b886cb8feaa4f9
Ryan Ahler <rahler@bsu.edu>
2019-02-21 10:40:51 -0500

    Fixed class path

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,6 +1,6 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialTest {
 
